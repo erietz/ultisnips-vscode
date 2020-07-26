4f19ad73792ee4a4bd2587ba9994f5174d05d42b
Ethan Rietz <ewrietz@gmail.com>
2020-07-25 21:49:48 -0500

    allow for extended filetypes

---
+++ scripts/vscode_filetypes.py
@@ -0,0 +1,9 @@
+
+languages = {
+    ("markdown", "md"): "markdown",
+    ("python", "py"): "python",
+    ("json"): "json",
+    ("sh", "zsh", "tcsh", "csh", "ksh"): "shellscript",
+    ("tex", "latex"): "tex",
+    ("yaml"): "yaml"
+}
