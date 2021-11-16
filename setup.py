63fd9924813af917e82b13374479a80f3219bbf5
Ethan Rietz <ewrietz@gmail.com>
2021-11-16 14:59:53 -0600

    rename files

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name="ultisnips_to_vscode",
+    name="ultisnips-to-vscode",
     version="0.0.3",
     description="Converts a directory of ultisnips snippets to json for vscode",
     author="Ethan Rietz",
     keywords="vim ultisnips snippets vscode json",
-    packages=['languages'],
-    scripts=['scripts/ultisnips_to_vscode']
+    packages=['src'],
+    scripts=['scripts/ultisnips2vscode']
 )
 
