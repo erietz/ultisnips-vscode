5d2a4c09a66b33be56333e069d56a7e6c4cf95e4
Ethan Rietz <ewrietz@gmail.com>
2021-12-21 16:12:14 -0600

    create unittest for python snippets file

--- test_ultisnips_parser.py
+++ test_ultisnips_parser.py
@@ -2,13 +2,61 @@ import unittest
 from pathlib import Path
 from src import UltisnipParser
 
-class Foo(unittest.TestCase):
-    def test_test(self):
+class TestParseSnippet(unittest.TestCase):
+    def test_parsing_a_single_file(self):
         parser = UltisnipParser(
             Path("./test/ultisnips_snippets"),
             Path("./test/vscode_snippets/")
         )
 
         snippets = parser._parse_snippet(Path("./test/ultisnips_snippets/python.snippets"))
-        print(snippets)
 
+        self.assertDictEqual(
+            snippets.get("main"),
+            {
+                "prefix": "main",
+                "description": "main block",
+                "body": [
+                    "if __name__ == '__main__':",
+                    "    pass"
+                ]
+            }
+        )
+
+        self.assertDictEqual(
+            snippets.get("modules-math"),
+            {
+                "prefix": "modules-math",
+                "description": "import math modules",
+                "body": [
+                    "import numpy as np",
+                    "import matplotlib.pyplot as plt",
+                    "import pandas as pd"
+                ]
+            }
+        )
+
+        self.assertDictEqual(
+            snippets.get("shebang"),
+            {
+                "prefix": "shebang",
+                "description": "shebang",
+                "body": [
+                    "#!/usr/bin/env python3",
+                    "$0"
+                ]
+            }
+        )
+
+        # NOTE: this snippet does not contain the optional description
+        self.assertDictEqual(
+            snippets.get("delim"),
+            {
+                "prefix": "delim",
+                "body": [
+                    "# In[$1]:",
+                    "",
+                    "$0"
+                ]
+            }
+        )
